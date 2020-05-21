06cff517fe177a6deb03fd83ae9bdafed7f1ad06
David <bderazo@espe.edu.ec>
2020-05-21 14:14:19 -0500

    Codigo inicial

---
+++ src/main/java/com/erazo/ejercicio1/PryEjercicio1Application.java
@@ -0,0 +1,13 @@
+package com.erazo.ejercicio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PryEjercicio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PryEjercicio1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/erazo/ejercicio1/PryEjercicio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erazo.ejercicio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PryEjercicio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
